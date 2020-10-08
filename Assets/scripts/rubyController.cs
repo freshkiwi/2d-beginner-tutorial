8cc4913fb6b99a48201224aff6e65af683666418
Keanu <kmartinez3@colum.edu>
2020-10-07 20:14:59 -0500

    movement + world design
    added controls to move with arrow keys
    design a world with tiles and environment objects

---
+++ Assets/scripts/rubyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class rubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime; 
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;      
+    }
+}
