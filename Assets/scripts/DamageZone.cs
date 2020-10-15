9c76d9f8ea4debd198d3777325b66a3576d68e61
Keanu <kmartinez3@colum.edu>
2020-10-15 01:42:41 -0500

    Full world design
    added:
    - movement blocs
    - collectable health item
    - damage zones
    - enemies

---
+++ Assets/scripts/DamageZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageZone : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        rubyController controller = other.GetComponent<rubyController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(-1);
+        }
+    }
+
+}
